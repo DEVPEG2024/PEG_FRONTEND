276567d319817f10474036c92d19e470a5558a26
mguldner <mathieuguldner.emn@gmail.com>
2024-10-17 20:26:32 +0000

    Empêcher modification des réponses au formulaire en consultation depuis vue admin

--- src/views/app/admin/forms/builder/components/fields/select.tsx
+++ src/views/app/admin/forms/builder/components/fields/select.tsx
@@ -1,10 +1,10 @@
 import { Select } from '@/components/ui'
 
-function SelectSection({className, label, placeholder, options, value, onChange}: {className: string, label: string, placeholder: string, options: any, value: string, onChange: Function}) {
+function SelectSection({className, label, placeholder, options, value, onChange, disabled = false}: {className: string, label: string, placeholder: string, options: any, value: string, onChange: Function, disabled?: boolean}) {
   return (
     <div className={className}>
       <p className="text-sm text-gray-400 mb-2">{label}</p>
-      <Select options={options} placeholder={placeholder} onChange={(e) => onChange(e)} {...(value && { value })}/>
+      <Select options={options} placeholder={placeholder} onChange={(e) => onChange(e)} {...(value && { value })} isDisabled={disabled}/>
     </div>
   );
 }
