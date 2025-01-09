2a3c78162b2513c69b10735f4fa146b0ad0d2028
mguldner <mathieuguldner.emn@gmail.com>
2025-01-09 19:56:19 +0000

    Passage HTTP temporaire

--- src/configs/env.config.ts
+++ src/configs/env.config.ts
@@ -4,7 +4,7 @@ const dev = {
   
   // API HEROKU
   const prod = {
-    API_ENDPOINT_URL: 'https://api.mypeg.fr'
+    API_ENDPOINT_URL: 'http://api.mypeg.fr' // TODO: passer en HTTPS via Caddy par exemple (https://forum.strapi.io/t/caddy-proxying-with-strapi/40616) --> voir page déploiement Strapi
   };
   
   const getEnv = () => {
