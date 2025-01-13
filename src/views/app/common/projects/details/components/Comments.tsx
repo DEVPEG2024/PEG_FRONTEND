749932a8b6235a746a3d0d85b8b61613831cad82
mguldner <mathieuguldner.emn@gmail.com>
2025-01-13 11:10:46 +0000

    Gestion visibilité sur commentaire + accès modification tache

--- src/views/app/common/projects/details/components/Comments.tsx
+++ src/views/app/common/projects/details/components/Comments.tsx
@@ -16,14 +16,20 @@ import dayjs from 'dayjs';
 import { User } from '@/@types/user';
 import { Image } from '@/@types/image';
 import { apiUploadFile } from '@/services/FileServices';
-import { Upload } from '@/components/ui';
+import { Select, Upload } from '@/components/ui';
+import { ADMIN, SUPER_ADMIN } from '@/constants/roles.constant';
+import { visibilityData } from '../../lists/constants';
+import { hasRole } from '@/utils/permissions';
 
 type TimelineCommentProps = TimeLineItemProps & {
   comment: Comment;
+  user: User;
 };
 
+// TODO SUITE: Continuer et vérifier si ok en testant
 const Comments = () => {
   const [commentText, setCommentText] = useState<string>('');
+  const [visibility, setVisibility] = useState<string>('all');
   const [images, setImages] = useState<Image[]>([])
   const dispatch = useAppDispatch();
   const {user}: {user: User} = useRootAppSelector((state: RootState) => state.auth.user);
@@ -47,6 +53,7 @@ const Comments = () => {
         content: commentText,
         user: user,
         images: newImages.map(({id}) => id),
+        visibility: user.role.name === 'customer' ? 'customer' : user.role.name === 'producer' ? 'producer' : visibility,
       };
 
       dispatch(createComment({comment, project}))
@@ -102,6 +109,14 @@ const Comments = () => {
     return valid;
   };
 
+  const determineVisibleComments = (comments: Comment[], user: User): Comment[] => {
+    if (hasRole(user, [SUPER_ADMIN, ADMIN])) {
+      return comments
+    } else {
+      return comments.filter(({visibility}) => visibility === 'all' || visibility === user.role.name)
+    }
+  }
+
   return (
     <Container className="h-full">
       <div className="grid md:grid-cols-1 lg:grid-cols-3 gap-4">
@@ -110,10 +125,11 @@ const Comments = () => {
             <div>
               <h4>Commentaires</h4>
               <Timeline>
-                {comments.map((comment) => (
+                {determineVisibleComments(comments, user).map((comment) => (
                   <TimelineComment
                     key={comment.documentId}
                     comment={comment}
+                    user={user}
                     isLast={
                       comment.documentId ===
                       comments[comments.length - 1].documentId
@@ -121,37 +137,54 @@ const Comments = () => {
                   />
                 ))}
               </Timeline>
-              <div className="mt-6 mb-3 flex flex-auto">
-                <Avatar size={30} shape="circle" icon={<HiUserCircle />} />
-                <div className="ml-4 rtl:mr-4 w-full">
-                  <Input
-                    onChange={(e) => setCommentText(e.target.value)}
-                    value={commentText}
-                    textArea
-                    placeholder="Ajouter un commentaire"
-                  />
+              <Card className="mt-6">
+                <div className="mt-1 mb-3 flex flex-auto gap-4">
+                  <Avatar size={30} shape="circle" icon={<HiUserCircle />} />
+                  <div className="w-full">
+                    <Input
+                      onChange={(e) => setCommentText(e.target.value)}
+                      value={commentText}
+                      textArea
+                      placeholder="Ajouter un commentaire"
+                    />
+                  </div>
                 </div>
-              </div>
-              <div className="flex flex-row items-center justify-end gap-2">
-                <Upload
-                  multiple
-                  showList
-                  draggable
-                  uploadLimit={4}
-                  beforeUpload={beforeUpload}
-                  onFileAdd={(file) =>
-                    onFileAdd(file)
-                  }
-                  onFileRemove={(file) =>
-                    onFileRemove(file)
-                  }
-                  field={{ name: 'images' }}
-                  fileList={images.map(({file}) => file)}
-                />
-                <Button variant="solid" onClick={submitComment} loading={loading}>
-                  Ajouter
-                </Button>
-              </div>
+                <div className="flex flex-row items-center justify-between gap-2 mb-4">
+                  <Upload
+                    multiple
+                    showList
+                    draggable
+                    uploadLimit={4}
+                    beforeUpload={beforeUpload}
+                    onFileAdd={(file) =>
+                      onFileAdd(file)
+                    }
+                    onFileRemove={(file) =>
+                      onFileRemove(file)
+                    }
+                    field={{ name: 'images' }}
+                    fileList={images.map(({file}) => file)}
+                  />
+                  {hasRole(user, [SUPER_ADMIN, ADMIN]) && (
+                    <div className="flex flex-row self-start w-1/2 items-center gap-4">
+                      <span className="w-max">Visibilité :</span>
+                      <Select
+                        size="sm"
+                        className="w-3/4 justify-self-start"
+                        placeholder={'Visibilité'}
+                        options={visibilityData}
+                        value={visibilityData.find(({value}) => value === visibility)}
+                        onChange={(e: any) => {
+                          setVisibility(e.value);
+                        }}
+                      />
+                    </div>
+                  )}
+                  <Button variant="solid" onClick={submitComment} loading={loading}>
+                    Ajouter
+                  </Button>
+                </div>
+              </Card>
             </div>
           </AdaptableCard>
         </div>
@@ -163,14 +196,31 @@ const Comments = () => {
 
 const TimelineComment = ({
   comment,
+  user,
   ...rest
 }: TimelineCommentProps) => {
-  const userName = `${comment.user.firstName} ${comment.user.lastName}`;
+  const determineAuthorRoleLabel = (user: User): string => {
+    switch (user.role.name) {
+      case 'customer':
+        return 'Client ' + user.customer?.name;
+      case 'producer':
+        return 'Producteur ' + user.producer?.name;
+      case 'super_admin':
+        return 'Administrateur';
+      default:
+        return '';
+    }
+  }
+  const authorLabel = `${comment.user.firstName} ${comment.user.lastName} (${determineAuthorRoleLabel(comment.user)})`;
   const dispatch = useAppDispatch();
 
   const handleDeleteComment = async () => {
     dispatch(deleteComment(comment.documentId))
   };
+
+  const determineCommentVisibility = (visibility: string): string => {
+    return visibilityData.find(({value}) => value === visibility)?.label || visibilityData.find(({value}) => value === 'all')!.label;
+  }
 
   return (
     <Timeline.Item
@@ -181,13 +231,16 @@ const TimelineComment = ({
       <div className="my-1 flex items-center justify-between">
         <span>
           <span className="font-semibold text-gray-900 dark:text-gray-100">
-            {userName}
+            {authorLabel}
           </span>
           <span className="mx-2">a ajouté un commentaire </span>
           <span className="text-gray-500 text-end">
             le {dayjs(comment.createdAt).format('DD/MM/YYYY à HH:mm')}
           </span>
         </span>
+        {hasRole(user, [SUPER_ADMIN, ADMIN]) && (
+          <span>{`Visibilité : ${determineCommentVisibility(comment.visibility)}`}</span>
+        )}
       </div>
       <div className="grid grid-cols-12 gap-4 mt-4">
         <Card
