337eac8cca4ed2153576dcc17d5c177bcba36b4e
mguldner <mathieuguldner.emn@gmail.com>
2025-01-29 13:28:59 +0100

    Revert "Ajout d'un cache-buster dans la configuration de Vite"
    This reverts commit 753c67b33341f8b26be1812db98651df18634a36.

--- vite.config.ts
+++ vite.config.ts
@@ -29,12 +29,5 @@ export default defineConfig({
     commonjsOptions: {
       requireReturnsDefault: 'auto',
     },
-    rollupOptions: {
-      output: {
-        assetFileNames: "assets/[name].[hash][extname]",
-        chunkFileNames: "assets/[name].[hash].js",
-        entryFileNames: "assets/[name].[hash].js"
-      }
-    }
   },
 });
