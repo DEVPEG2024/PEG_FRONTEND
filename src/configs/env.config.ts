27c2248613b39846ce1f2f1c89fefb2f17bf97b7
mguldner <mathieuguldner.emn@gmail.com>
2025-04-08 11:34:00 +0000

    Ajout de la public live key pour Stripe

--- src/configs/env.config.ts
+++ src/configs/env.config.ts
@@ -7,7 +7,7 @@ const dev = {
   // API HEROKU
   const prod = {
     API_ENDPOINT_URL: 'https://api.mypeg.fr',
-    STRIPE_PUBLIC_KEY: 'pk_test_51R9MNCQMnon3CcoqBv9jtL5Njxa36ATMCBqNnDZVuBmnBBtt990Qs4lCm8cBPBKVtSPs47EdJgKojXIYlpd6VaE300SZEcS93P' 
+    STRIPE_PUBLIC_KEY: 'pk_live_51R9MMyKa36UjT6qO6418qHBIJuOqvtIXK9VIUD1H7DV9wUVG9SYyHKPDPkiC4PfgINSqzUy5bWWIE9viuwuBKMjk00RO7QspVZ' 
   };
   
   const getEnv = () => {
