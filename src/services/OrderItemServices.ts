20a8b1c929e086f916d60e742d044ec43711effe
mguldner <mathieuguldner.emn@gmail.com>
2025-04-09 11:23:58 +0000

    Suppression des OrderItem et FormAnswer en cas d'échec de paiement

--- src/services/OrderItemServices.ts
+++ src/services/OrderItemServices.ts
@@ -59,7 +59,7 @@ export type GetOrderItemsResponse = {
 export async function apiGetOrderItems(data: GetOrderItemsRequest = {pagination: {page: 1, pageSize: 1000}, searchTerm: ''}): Promise<AxiosResponse<ApiResponse<{orderItems_connection: GetOrderItemsResponse}>>> {
     const query = `
     query getOrderItems($searchTerm: String, $pagination: PaginationArg) {
-        orderItems_connection(filters: {product: {name: {containsi: $searchTerm}}}, pagination: $pagination) {
+        orderItems_connection(filters: {product: {name: {containsi: $searchTerm}}}, pagination: $pagination, sort: "createdAt:desc") {
             nodes {
                 documentId
                 price
--- src/views/app/customer/cart/Cancel.tsx
+++ src/views/app/customer/cart/Cancel.tsx
@@ -1,9 +1,32 @@
 import { Container } from '@/components/shared';
 import { Button } from '@/components/ui';
+import { API_BASE_URL } from '@/configs/api.config';
+import { TOKEN_TYPE } from '@/constants/api.constant';
+import { useAppSelector } from '@/store';
+import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 function Cancel() {
   const navigate = useNavigate();
+  const { token } = useAppSelector((state) => state.auth.session)
+
+  useEffect(() => {
+    const cancelOrderItems = async () => {
+      const sessionId = new URLSearchParams(window.location.search).get('session_id');
+      if (sessionId) {
+        await fetch(API_BASE_URL + '/checkout/cancel', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `${TOKEN_TYPE}${token}`
+          },
+          body: JSON.stringify({sessionId}),
+        })
+      }
+    }
+    
+    cancelOrderItems()
+  }, []);
 
   return (
     <Container className="h-full">
@@ -11,7 +34,7 @@ function Cancel() {
       <h1 className="text-2xl font-bold text-red-600">Paiement annulé</h1>
       <p>Vous pouvez réessayer à tout moment.</p>
       <Button
-        onClick={() => navigate('/common/cart')}
+        onClick={() => navigate('/customer/cart')}
         className="mt-4 px-4 py-2 bg-gray-500 text-white rounded"
         variant="solid"
       >
