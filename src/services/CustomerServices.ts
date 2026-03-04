49af5c5cd2d5308d9e189d43aeafb15f5d1301f6
anthony saldi <zoomproject@air-de-anthony.home>
2026-03-04 12:38:43 +0100

    fix: remove duplicate /api prefix

--- src/services/CustomerServices.ts
+++ src/services/CustomerServices.ts
@@ -11,7 +11,7 @@ export const apiUploadFile = (file: File) => {
   const form = new FormData()
   form.append('files', file)
   return ApiService.fetchData({
-    url: `/api/upload`,
+    url: `/upload`,
     method: 'post',
     data: form,
     headers: { 'Content-Type': 'multipart/form-data' },
@@ -29,17 +29,34 @@ export const apiGetCustomers = (params: GetCustomersRequest) => {
   if (searchTerm) query.set('filters[name][$containsi]', searchTerm)
   query.set('populate[customerCategory]', 'true')
   query.set('populate[logo]', 'true')
-  return ApiService.fetchData({ url: `/api/customers?${query.toString()}`, method: 'get' })
+  return ApiService.fetchData({
+    url: `/customers?${query.toString()}`,
+    method: 'get',
+  })
 }
 
 export const apiGetCustomerForEditById = (id: string) =>
-  ApiService.fetchData({ url: `/api/customers/${id}?populate=deep`, method: 'get' })
+  ApiService.fetchData({
+    url: `/customers/${id}?populate=deep`,
+    method: 'get',
+  })
 
 export const apiDeleteCustomer = (id: string) =>
-  ApiService.fetchData({ url: `/api/customers/${id}`, method: 'delete' })
+  ApiService.fetchData({
+    url: `/customers/${id}`,
+    method: 'delete',
+  })
 
 export const apiCreateCustomer = (data: any) =>
-  ApiService.fetchData({ url: `/api/customers`, method: 'post', data: { data } })
+  ApiService.fetchData({
+    url: `/customers`,
+    method: 'post',
+    data: { data },
+  })
 
 export const apiUpdateCustomer = (id: string, data: any) =>
-  ApiService.fetchData({ url: `/api/customers/${id}`, method: 'put', data: { data } })
+  ApiService.fetchData({
+    url: `/customers/${id}`,
+    method: 'put',
+    data: { data },
+  })
