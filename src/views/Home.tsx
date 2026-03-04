b035d204b014b8d3581a22a30d05460d0762ce01
anthony saldi <zoomproject@MacBook-Air-de-anthony.local>
2026-03-04 03:47:31 +0100

    debug: show authority

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,35 +1,40 @@
-import { User } from '@/@types/user';
-import { AuthorityCheck } from '@/components/shared';
-import { useAppSelector } from '@/store';
-import DashboardAdmin from '@/views/app/admin/home/DashboardAdmin';
-import DashboardCustomer from '@/views/app/customer/home/DashboardCustomer';
-import DashboardProducer from '@/views/app/producer/home/DashboardProducer';
-import { Suspense } from 'react';
+import { User } from '@/@types/user'
+import { AuthorityCheck } from '@/components/shared'
+import { useAppSelector } from '@/store'
+import DashboardAdmin from '@/views/app/admin/home/DashboardAdmin'
+import DashboardCustomer from '@/views/app/customer/home/DashboardCustomer'
+import DashboardProducer from '@/views/app/producer/home/DashboardProducer'
+import { Suspense } from 'react'
 
 const Home = () => {
-  const { user }: { user: User } = useAppSelector((state) => state.auth.user);
-  return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <AuthorityCheck
-        authority={['super_admin']}
-        userAuthority={user?.authority || []}
-      >
-        <DashboardAdmin />
-      </AuthorityCheck>
-      <AuthorityCheck
-        authority={['customer']}
-        userAuthority={user?.authority || []}
-      >
-        <DashboardCustomer />
-      </AuthorityCheck>
-      <AuthorityCheck
-        authority={['producer']}
-        userAuthority={user?.authority || []}
-      >
-        <DashboardProducer />
-      </AuthorityCheck>
-    </Suspense>
-  );
-};
+  const { user }: { user: User } = useAppSelector((state) => state.auth.user)
 
-export default Home;
+  const userAuthority =
+    (user as any)?.authority ||
+    (user as any)?.user?.authority ||
+    (user as any)?.user?.user?.authority ||
+    []
+console.log("USER AUTHORITY =", userAuthority)
+  return (
+  <Suspense fallback={null}>
+
+    <div style={{ padding: 10, color: "white" }}>
+      AUTH: {JSON.stringify(userAuthority)}
+    </div>
+
+    <AuthorityCheck authority={['super_admin']} userAuthority={userAuthority}>
+      <DashboardAdmin />
+    </AuthorityCheck>
+
+    <AuthorityCheck authority={['customer']} userAuthority={userAuthority}>
+      <DashboardCustomer />
+    </AuthorityCheck>
+
+    <AuthorityCheck authority={['producer']} userAuthority={userAuthority}>
+      <DashboardProducer />
+    </AuthorityCheck>
+
+  </Suspense>
+)
+}
+export default Home
